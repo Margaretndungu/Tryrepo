a1d0b7dddfaf2df963ca1244527bb446298172c7
Margaretndungu <mnjoki.ndungu05@gmail.com>
2023-04-17 14:47:20 -0700

    transaction

---
+++ system.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+// Define structs to represent bank accounts and transactions
+typedef struct {
+    int accountNumber;
+    char accountHolder[50];
+    double balance;
+    // Add other account attributes as needed
+} BankAccount;
+
+typedef struct {
+    int transactionID;
+    int accountNumber;
+    char transactionType[10];
+    double amount;
+    // Add other transaction attributes as needed
+} Transaction;
+
+// Define functions to manage bank accounts
+void createAccount(BankAccount* account) {
+    // Implement logic to create a new bank account
+}
+
+void displayAccount(BankAccount* account) {
+    // Implement logic to display bank account details
+}
+
+void deposit(BankAccount* account, double amount) {
+    // Implement logic to deposit amount into a bank account
+}
+
+void withdraw(BankAccount* account, double amount) {
+    // Implement logic to withdraw amount from a bank account
+}
+
+// Define functions to manage transactions
+void recordTransaction(Transaction* transaction) {
+    // Implement logic to record a transaction
+}
+
+void displayTransaction(Transaction* transaction) {
+    // Implement logic to display transaction details
+}
+
+// Main function
+int main() {
+    BankAccount account; // Create a bank account
+    createAccount(&account);
+
+    // Perform transactions
+    deposit(&account, 1000.0);
+    withdraw(&account, 500.0);
+
+    // Display account details and transactions
+    displayAccount(&account);
+    displayTransaction(transaction1);
+    displayTransaction(transaction2);
+
+    return 0;
+}
+
